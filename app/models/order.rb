baf0b702b0e966b48c6e79629b4698cc9f6da9d4
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2024-06-04 14:29:53 -0300

    implementa fluxo de status do pedido

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,36 @@ class Order < ApplicationRecord
 
   STATUS = ["pending", "processing", "confirmed", "en_route", "ready_for_pickup"]
   validates :status, inclusion: { in: STATUS }
+
+  def process_items
+    if self.status == "pending"
+      self.update(status: "processing")
+    else
+      raise "O pedido não pôde ser separado, pois não está pendente!"
+    end
+  end
+
+  def confirm_items
+    if self.status == "processing"
+      self.update(status: "confirmed")
+    else
+      raise "O pedido não pôde ser confirmado, pois não foi separado!"
+    end
+  end
+
+  def ship_order
+    if self.status == "confirmed"
+      self.update(status: "en_route")
+    else
+      raise "O pedido não pôde ser colocado em rota de entrega, pois não foi confirmado!"
+    end
+  end
+
+  def make_ready_for_pickup
+    if self.status == "confirmed"
+      self.update(status: "ready_for_pickup")
+    else
+      raise "O pedido não está disponível para ser colocado para retirada, pois não foi confirmado!"
+    end
+  end
 end
