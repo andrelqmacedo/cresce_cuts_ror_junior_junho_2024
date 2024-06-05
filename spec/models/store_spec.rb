331749d85fd6207150e06039317343b71068dc19
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2024-06-05 11:31:00 -0300

    adiciona testes para model store

---
+++ spec/models/store_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Store, type: :model do
+  context "associations" do
+    it { should have_many(:orders) }
+  end
+
+  context "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:address) }
+
+    it { should validate_length_of(:name) }
+  end
+end
