159b46ee23f50d139e00444c4a63e0869f5d9ecd
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2024-06-05 11:23:19 -0300

    instala gem shoulda-matchers e refatora testes de customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,43 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   context "associations" do
-    it 'has_many :orders' do
-      expect(Customer.reflect_on_association(:orders)).to_not be nil
-    end
-    it 'has_many :carts' do
-      expect(Customer.reflect_on_association(:carts)).to_not be nil
-    end
+    it { should have_many(:orders) }
+    it { should have_many(:carts) }
   end
+
   context "validations" do
-    it 'validate presence of name' do
-      customer = Customer.create(email: "teste@teste.com", address: "Rua das Flores, 123")
-      expect(customer).to_not be_valid
-      expect(customer.errors[:name]).to include("can't be blank")
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
 
-    it 'validate presence of email' do
-      customer = Customer.create(name: 'Teste', address: 'Rua das Flores, 123')
-      expect(customer).not_to be_valid
-      expect(customer.errors[:email]).to include("can't be blank")
-    end
-
-    it 'validate presence of address' do
-      customer = Customer.create(name: 'Teste', email: 'Rua das Flores, 123')
-      expect(customer).not_to be_valid
-      expect(customer.errors[:address]).to include("can't be blank")
-    end
-
-    it 'validate uniqueness of email' do
-      existing_customer = Customer.create(email: 'teste@teste.com', name: 'Teste', address: 'Rua das Rosas, 456')
-      new_customer = Customer.create(name: 'Teste', email: 'teste@teste.com', address: 'Rua das Flores, 123')
-      expect(new_customer).not_to be_valid
-      expect(new_customer.errors[:email]).to include('has already been taken')
-    end
-
-    it 'validate format of email' do
-      customer = Customer.create(name: 'Test', email: 'invalid_email', address: 'Rua das Flores, 123')
-      expect(customer).not_to be_valid
-      expect(customer.errors[:email]).to include('is invalid')
-    end
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value('example@example.com').for(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
   end
 end
