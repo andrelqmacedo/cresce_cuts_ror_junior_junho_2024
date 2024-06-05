1571ecd7355fd51f4ee4d622568a6ea9d4301ae8
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2024-06-05 11:42:55 -0300

    adiciona testes para model cart

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Cart, type: :model do
+  context "associations" do
+    it { should belong_to(:customer) }
+    it { should have_many(:cart_items) }
+    it { should have_many(:items).through(:cart_items) }
+  end
+
+  context "validations" do
+    it { should validate_presence_of(:customer) }
+  end
+end
