1336496f1b78db0e41fa33afd41fa55ba25ecddf
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2024-06-06 09:31:46 -0300

    cria service para atualizacao do status do pedido

--- app/models/order.rb
+++ app/models/order.rb
@@ -18,31 +18,9 @@ class Order < ApplicationRecord
     OrderTotalCalculatorService.new(self).calculate
   end
 
-#   def update_status(new_status)
-#     update(status: new_status)
-#     case new_status
-#     when "processing"
-#       puts "A loja aceitou seu pedido! Pedido em separação!"
-#     when "confirmed"
-#       if all_items_present?
-#         puts "Os itens escolhidos foram separados! Pedido confirmado!"
-#       else
-#         raise "O pedido não pôde ser confirmado, pois não foi separado!"
-#       end
-#     when "en_route"
-#       if payment_status == "paid"
-#         puts "Pagamento recebido! O pedido está em rota de entrega!"
-#       else
-#         raise "O pedido não pôde ser colocado em rota de entrega, pois não foi confirmado!"
-#       end
-#     when "ready_for_pickup"
-#       if payment_status == "paid"
-#         puts "Pagamento recebido! O pedido está disponível para retirada!"
-#       else
-#         raise "O pedido não está disponível para retirada, pois não foi confirmado!"
-#       end
-#     end
-#   end
+  def update_status(new_status)
+    OrderUpdaterService.new(self).update_status(new_status)
+  end
 
 #   private
 
---
+++ app/services/order_updater_service.rb
@@ -0,0 +1,39 @@
+class OrderUpdaterService
+  def initialize(order)
+    @order = order
+  end
+
+  def update_status(new_status)
+    @order.update(status: new_status)
+
+    case new_status
+
+    when "processing"
+      puts "A loja aceitou seu pedido! Pedido em separação!"
+    when "confirmed"
+      if all_items_present?
+        puts "Os itens escolhidos foram separados! Pedido confirmado!"
+      else
+        raise "O pedido não pôde ser confirmado, pois não foi separado!"
+      end
+    when "en_route"
+      if @order.payment_status == "paid"
+        puts "Pagamento recebido! O pedido está em rota de entrega!"
+      else
+        raise "O pedido não pôde ser colocado em rota de entrega, pois não foi confirmado!"
+      end
+    when "ready_for_pickup"
+      if @order.payment_status == "paid"
+        puts "Pagamento recebido! O pedido está disponível para retirada!"
+      else
+        raise "O pedido não está disponível para retirada, pois não foi confirmado!"
+      end
+    end
+  end
+
+  private
+
+  def all_items_present?
+    @order.order_items.all? { |item| item.present? }
+  end
+end
